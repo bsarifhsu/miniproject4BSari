3a0dfb5a17dff3ba77c66a6f68170d42fd5bd5f7
bsarifhsu <b_sari@mail.fhsu.edu>
2023-11-04 00:51:27 -0500

    Product Details added.

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "inventory"
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    # path('product/<int:product_id>/edit/', views.product_edit, name='product_edit'),
+    # path('product/<int:product_id>/delete/', views.product_delete, name='product_delete'),
     # Add more URL patterns for other views as needed
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
 
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'inventory/product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'inventory/product_detail.html', {'product': product})
