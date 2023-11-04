116ef4871ac68bb9773b112649457fcf99208f97
bsarifhsu <b_sari@mail.fhsu.edu>
2023-11-03 23:33:56 -0500

    Models registered.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
+from .models import CheckOut
+from .models import CheckIn
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(CheckOut)
+admin.site.register(CheckIn)
+
